f9ce62da3335b89bba76b508d12377baaccc3666
Leandro Vivaldini <LeandroVivaldini@users.noreply.github.com>
2024-03-12 20:42:23 -0300

    Aula 12/03/24

---
+++ controllers/usuarios.js
@@ -0,0 +1,12 @@
+const { json } = require('express');
+const db = require('../database/connection');
+
+module.exports = {
+    async listarMesas(request,response) {
+        try {
+            return response.status(200).json({confirma:'Listar Mesas'})
+        } catch (error) {
+            
+        }
+    }
+}
